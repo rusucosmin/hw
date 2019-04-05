116717894a014bb10a9b7e27bb79278953d30717
Cosmin Rusu <cr.rusucosmin@gmail.com>
2019-04-05 15:23:33 +0200

    Added join

--- src/app.py
+++ src/app.py
@@ -6,6 +6,13 @@ from operator import add
 from pyspark.sql import SparkSession
 import os
 
+def split_topic_row(r):
+  sp = r.value.split()
+  return (sp[1], sp[0])
+
+def split_vectors_train_row(r):
+  sp = r.value.split()
+  return (sp[0], sp[1:])
 
 if __name__ == "__main__":
     DATASET_DIR = '/data/datasets/'
@@ -17,8 +24,14 @@ if __name__ == "__main__":
         .appName("PysparkHogwildV1")\
         .getOrCreate()
 
-    topic_rid_rdd = spark.read.text(TOPICS_FILE).rdd
-    vectors_train_rdd = spark.read.text(VECTORS_TRAIN_FILE).rdd
+    topic_rid_rdd = (spark.read.text(TOPICS_FILE).rdd
+      .map(split_topic_row))
+    vectors_train_rdd = (spark.read.text(VECTORS_TRAIN_FILE).rdd
+      .map(split_vectors_train_row))
+
+    print(topic_rid_rdd
+        .join(vectors_train_rdd)
+        .count())
 
     print(topic_rid_rdd.count())
     print(topic_rid_rdd.take(10))

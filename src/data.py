5e4ee0ba021ab4ceae5cc360e815b0b29702909c
Manuel Cherep <manuelcherep@gmail.com>
2019-04-13 17:37:01 +0200

    Load train and test datasets

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
-from settings import (DATASET_DIR, TOPICS_FILE, TRAIN_FILE, VAL_SPLIT)
+from settings import (DATASET_DIR, TOPICS_FILE,
+                      TRAIN_FILE, TEST_FILES, VAL_SPLIT)
 
 
 def load_train(spark):
@@ -7,31 +8,57 @@ def load_train(spark):
                  .map(split_topic_row)).toDF(['reuters_id', 'topic_tag'])
     # Read train dataset
     raw_train_df = (spark.read.text(TRAIN_FILE).rdd
-                    .map(split_train_row)).toDF(['reuters_id', 'features'])
+                    .map(split_data_row)).toDF(['reuters_id', 'features'])
 
+    train_df = preprocess(topics_df, raw_train_df)
+
+    train_df.printSchema()
+
+    val_df, train_df = \
+        train_df.randomSplit([VAL_SPLIT, 1-VAL_SPLIT], seed=24)
+
+    return (val_df, train_df)
+
+
+def load_test(spark):
+    # Read topics dataset
+    topics_df = (spark.read.text(TOPICS_FILE).rdd
+                 .map(split_topic_row)).toDF(['reuters_id', 'topic_tag'])
+
+    # Read test datasets
+    test_dfs = []
+    for TEST_FILE in TEST_FILES:
+        raw_test_df = (spark.read.text(TEST_FILE).rdd.map(split_data_row)) \
+            .toDF(['reuters_id', 'features'])
+        test_df = preprocess(topics_df, raw_test_df)
+        test_dfs += [test_df]
+
+    test_df = reduce(lambda df1, df2: df1.union(df2), test_dfs)
+
+    test_df.printSchema()
+
+    return test_df
+
+
+def preprocess(topics_df, data_df):
     # We want to add a column to the train dataset that represents the topic CCAT
     # (i.e. 1 if topic is CCAT; 0 otherwise). First, we join the train with the
     # topics.
-    rich_train_df = \
-        topics_df.join(raw_train_df, ['reuters_id'])
+    data_df = \
+        topics_df.join(data_df, ['reuters_id'])
 
     # 1. Add a boolean column that represents whether CCAT is the topic
     # 2. Group by reuters id and values, adding the targets which returns 1
     #    if CCAT is contained in the topics or 0 otherwise
-    # 3. Undo the sparsify matrix
-    rich_train_df = rich_train_df \
-        .withColumn('target', (rich_train_df.topic_tag == 'CCAT').cast('integer')) \
+    # 3. Sparse to dense matrix
+    data_df = data_df \
+        .withColumn('target', (data_df.topic_tag == 'CCAT').cast('integer')) \
         .groupBy(['reuters_id', 'features']).agg({'target': 'sum'}) \
         .rdd \
         .map(sparse_to_dense) \
         .toDF(['reuters_id', 'features', 'target'])
 
-    rich_train_df.printSchema()
-
-    val_df, train_df = \
-        rich_train_df.randomSplit([VAL_SPLIT, 1-VAL_SPLIT], seed=24)
-
-    return (val_df, train_df)
+    return data_df
 
 
 def split_topic_row(row):
@@ -42,8 +69,8 @@ def split_topic_row(row):
     return (cols[1], cols[0])
 
 
-def split_train_row(row):
-    # Split a row of the train dataset.
+def split_data_row(row):
+    # Split a row of the dataset.
     # The first value is the reuters id and the following are the features
     # encoded in a sparse matrix (i.e. column_idx:value)
     # Example: "2286 864:0.0497399253756197 1523:0.044664135988103 ..."

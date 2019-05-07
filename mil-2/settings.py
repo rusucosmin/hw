557ea9f8e8167182217c42b4fd7c610941b9f513
Manuel Cherep <manuelcherep@gmail.com>
2019-05-08 01:10:43 +0200

    Settings

--- mil-2/settings.py
+++ mil-2/settings.py
@@ -1,37 +1,18 @@
 import os
 
-LOCAL_PATH = os.path.join((os.sep)
-                          .join(os.path.dirname(os.path.abspath(__file__)).split(os.sep)[0:-2]),
-                          'hw/data/datasets')
+WORKERS = 3  # Number of workers
+LOCK = False  # Asynchronous with and without lock
+REG_LAMBDA = 1e-5  # Lambda for regularization
+EPOCHS = 1000  # Number of epochs to train
+BATCH = 100  # Batch size
+PERSISTENCE = 15  # Number of epochs of no change in loss as a stopping criteria
+LEARNING_RATE = 0.03 * (100 / BATCH) / WORKERS  # Learning rate
 
-# If testing locally, use localhost:port with different ports for each node/coordinator
-# When running on different machines, can use the same port for all.
-N_WORKERS = os.environ.get('N_WORKERS')
-RUNNING_WHERE = os.environ.get('WHERE') if os.environ.get('WHERE') == 'cluster' else 'local'
-DATA_PATH = os.environ.get('DATA_PATH') if os.environ.get('DATA_PATH') else LOCAL_PATH
-
-
-coordinator_address = 'coordinator-0.coordinator-service:80' if RUNNING_WHERE != 'local' else 'localhost:50051'
-worker_addresses = ['worker-{}.workers-service:80'.format(str(x)) for x in
-                    range(int(N_WORKERS))] if RUNNING_WHERE != 'local' else ['localhost:50052', 'localhost:50053',
-                                                                             'localhost:50054', 'localhost:50055']
-
-port = 80 if RUNNING_WHERE != 'local' else 50051
-
-TRAIN_FILE = os.path.join(DATA_PATH, 'lyrl2004_vectors_train.dat') if DATA_PATH else ''
-TOPICS_FILE = os.path.join(DATA_PATH, 'rcv1-v2.topics.qrels')
-TEST_FILES = [os.path.join(DATA_PATH, x) for x in ['lyrl2004_vectors_test_pt0.dat',
-                                                   'lyrl2004_vectors_test_pt1.dat',
-                                                   'lyrl2004_vectors_test_pt2.dat',
-                                                   'lyrl2004_vectors_test_pt3.dat']]
-
-running_mode = os.environ.get('RUNNING_MODE') if os.environ.get('RUNNING_MODE') else 'synchronous'
-synchronous = running_mode == 'synchronous'
-
-subset_size = 100  # Number of datapoints to train on each epoch
-# Learning rate for SGD. The term (100/subset_size) is used to adapt convergence to different subset sizes than 100
-learning_rate = 0.03 * (100 / subset_size) / len(worker_addresses)
-validation_split = 0.1  # Percentage of validation data
-epochs = 1000  # Number of training iterations over subset on each node
-persistence = 15  # Abort if after so many epochs learning rate does not decrease
-lambda_reg = 1e-5  # Regularization parameter
+DATASET_DIR = '../data/datasets/'
+TOPICS_FILE = os.path.join(DATASET_DIR, 'rcv1-v2.topics.qrels')
+TRAIN_FILE = os.path.join(DATASET_DIR, 'lyrl2004_vectors_train.dat')
+TEST_FILES = [os.path.join(DATASET_DIR, f) for f in ['lyrl2004_vectors_test_pt0.dat',
+                                                     'lyrl2004_vectors_test_pt1.dat',
+                                                     'lyrl2004_vectors_test_pt2.dat',
+                                                     'lyrl2004_vectors_test_pt3.dat']]
+VAL_SPLIT = 0.1
